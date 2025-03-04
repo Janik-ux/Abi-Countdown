b475c70f8ad1d4ec07be899bd7000907680c5362
Janik-ux <74071218+Janik-ux@users.noreply.github.com>
2025-03-04 10:26:01 +0100

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Apr 29, 2025 08:00:00").getTime();
+var datesDict = {
+"Deutsch": new Date("Apr 29, 2025 08:00:00").getTime(),
+"Russisch": new Date("Jun 3, 2025 08:00:00").getTime()
+};
 
 // Update the count down every 1 second
 var x = setInterval(function() {
 
   // Get today's date and time
   var now = new Date().getTime();
+
+  // select countdown date depending on subject
+  var countDownDate = datesDict.(document.getElementById("subject").value);
+  console.println(countDownDate);
     
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
